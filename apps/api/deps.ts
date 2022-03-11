e6ac07dfd3505bb5b53926958b000764a26f0491
Michael Tyson <michaeltyson@outlook.com>
2022-03-10 23:58:11 -0500

    begin orm exploration

--- apps/api/deps.ts
+++ apps/api/deps.ts
@@ -1,1 +1,7 @@
-export { Application } from "https://deno.land/x/oak@v10.4.0/mod.ts";
+export { Application } from 'https://deno.land/x/oak@v10.4.0/mod.ts';
+export {
+    Database,
+    DataTypes,
+    Model,
+    MySQLConnector,
+} from 'https://deno.land/x/denodb@v1.0.40/mod.ts';
--- apps/api/main.ts
+++ apps/api/main.ts
@@ -1,26 +1,73 @@
-import { Application } from "./deps.ts";
+import {
+    Application,
+    Database,
+    DataTypes,
+    Model,
+    MySQLConnector,
+} from './deps.ts';
+
+const connection = new MySQLConnector({
+    host: 'localhost',
+    username: 'deno_user',
+    password: 'deno_password',
+    database: 'deno_db',
+});
+
+const db = new Database(connection);
+
+class LogEntry extends Model {
+    static table = 'log_entries';
+    static timestamps = true;
+
+    static fields = {
+        id: { primaryKey: true, autoIncrement: true },
+        message: DataTypes.STRING,
+        json: {
+            type: DataTypes.JSON,
+            allowNull: true,
+        },
+    };
+
+    _id!: string;
+    message!: string;
+    json!: string;
+}
+
+db.link([LogEntry]);
+
+await db.sync({ drop: true });
 
 const app = new Application();
 
 // Logger
 app.use(async (ctx, next) => {
-  await next();
-  const rt = ctx.response.headers.get("X-Response-Time");
-  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+    await next();
+    const rt = ctx.response.headers.get('X-Response-Time');
+    console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
 });
 
 // Timing
 app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.response.headers.set('X-Response-Time', `${ms}ms`);
 });
 
-// Hello World!
-app.use((ctx) => {
-  const name = ctx.request.url.searchParams.get("name") ?? "World";
-  ctx.response.body = `Hello ${name}!`;
+// Hello World
+app.use(async (ctx) => {
+    const name = ctx.request.url.searchParams.get('name') ?? 'World';
+    const responseBody = `Hello ${name}!`;
+
+    const log = new LogEntry();
+    log.message = 'Hello World!';
+    log.json = JSON.stringify({
+        message: 'Hello World!',
+        name,
+    });
+    await log.save();
+
+    ctx.response.body = responseBody;
 });
 
 await app.listen({ port: 3000 });
